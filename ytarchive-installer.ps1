3eafcdefe21ac162e34a116279229c3d80f46a89
Haizi? <79714350+HaiziIzzudin@users.noreply.github.com>
2022-10-10 22:59:02 +0800

    ytarchive first try

--- ytarchive-installer.ps1
+++ ytarchive-installer.ps1
@@ -1,1 +1,15 @@
+Write-Host "Removing old ytarchive executable...";
+if (Test-Path -Path "~\AppData\Local\Microsoft\WindowsApps\ytarchive.exe") {
+    Remove-Item ~\AppData\Local\Microsoft\WindowsApps\ytarchive.exe;
+}
 
+Write-Host "Downloading latest ytarchive-windows.zip from kethsar/ytarchive github...";
+Invoke-WebRequest -Uri https://github.com/Kethsar/ytarchive/releases/download/latest/ytarchive_windows_amd64.zip -OutFile ~\AppData\Local\Microsoft\WindowsApps\ytarchive.zip;
+
+Write-Host "Extracting ytarchive.zip...";
+Expand-Archive -LiteralPath "~\AppData\Local\Microsoft\WindowsApps\ytarchive.zip" -DestinationPath "~\AppData\Local\Microsoft\WindowsApps\";
+
+Write-Host "Removing ytarchive.zip...";
+Remove-Item ~\AppData\Local\Microsoft\WindowsApps\ytarchive.zip;
+
+Write-Host "Installation done! Use by directly typing 'ytarchive'";
