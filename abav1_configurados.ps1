3ce0045740f3a35f3ebd6f030f5fe11acda756a0
Haizi? <79714350+HaiziIzzudin@users.noreply.github.com>
2022-11-25 08:42:11 +0800

    hohoho

--- abav1_configurados.ps1
+++ abav1_configurados.ps1
@@ -81,3 +81,4 @@ else {
 
 
 
+$fPth=Read-Host -Prompt "Drag & drop video file here, and press ENTER";Start-Transcript -Path $fPth -Append;$cqArr=@(21,22,23,24,25);foreach($cqVal in $cqArr){ab-av1 sample-encode -e libsvtav1 -i $fPth --crf $cqVal --preset 8 --samples 3 --vfilter "fps=30"};Stop-Transcript;
