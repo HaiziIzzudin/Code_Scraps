e3e30a240a857ecc24a1cd04804c8ac71da4b816
Nama diberi Haizi <79714350+HaiziIzzudin@users.noreply.github.com>
2022-12-15 01:30:48 +0800

    ffmpeg

--- ffmpeg-linux-rightway.sh
+++ ffmpeg-linux-rightway.sh
@@ -1,1 +1,5 @@
-
+sudo apt update && sudo apt upgrade -y;
+sudo add-apt-repository ppa:savoury1/ffmpeg4 -y;
+sudo add-apt-repository ppa:savoury1/ffmpeg5 -y;
+sudo apt update && sudo apt upgrade -y;
+sudo apt install ffmpeg;
